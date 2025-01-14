eac55785d81835fbf8625f9072e94bbb39054f34
Franckalv <franciscozaalvarez@gmail.com>
2025-01-14 17:07:37 -0600

    !Feat adding events page and some modals logic like modal hook

---
+++ src/components/eventsScreen/EventsModal.tsx
@@ -0,0 +1,33 @@
+import React, { ReactNode } from 'react';
+
+interface ModalProps {
+  isOpen: boolean;
+  closeModal: () => void;
+  children: ReactNode;
+}
+
+const EventsModal: React.FC<ModalProps> = ({ isOpen, closeModal, children }) => {
+  if (!isOpen) return null;
+
+  return (
+    <div
+      className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center"
+      onClick={closeModal}
+    >
+      <div
+        className="bg-white p-10 rounded-lg relative max-w-[80%] w-[600px] max-h-[80%] overflow-auto"
+        onClick={(e) => e.stopPropagation()}
+      >
+        <button
+          onClick={closeModal}
+          className="absolute top-2 right-2 text-xl font-bold text-gray-600"
+        >
+          X
+        </button>
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default EventsModal;
